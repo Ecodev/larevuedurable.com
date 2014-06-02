f840e8da3de954ec390acd5879b56d40166c7792
Sam Baptista <samuel.baptista@ecodev.ch>
2014-06-02 10:21:58 +0200

    Add robot.txt

--- bin/download.sh
+++ bin/download.sh
@@ -5,3 +5,4 @@ cd htdocs
 rsync -av larevuedurable.com:/sites/larevuedurable.com/htdocs/upload/ upload
 rsync -av larevuedurable.com:/sites/larevuedurable.com/htdocs/img/ img
 rsync -av larevuedurable.com:/sites/larevuedurable.com/htdocs/download/ download
+rsync -av larevuedurable.com:/sites/larevuedurable.com/htdocs/download_tmp/ download_tmp
