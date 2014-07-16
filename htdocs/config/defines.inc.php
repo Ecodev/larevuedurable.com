94b32a412bb89f9493c5f41de757a28149551780
Sam Baptista <samuel.baptista@ecodev.ch>
2014-07-16 15:05:32 +0200

    Set dev mode to false

--- htdocs/config/defines.inc.php
+++ htdocs/config/defines.inc.php
@@ -24,7 +24,7 @@
 *  International Registered Trademark & Property of PrestaShop SA
 */
 /* Debug only */
-define('_PS_MODE_DEV_', true);
+define('_PS_MODE_DEV_', false);
 if (_PS_MODE_DEV_)
 {
 	@ini_set('display_errors', 'on');	
