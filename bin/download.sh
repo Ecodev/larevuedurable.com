b75c059a1bfb961a36209b99e885473c4a8c28f0
Sam Baptista <samuel.baptista@ecodev.ch>
2014-12-10 15:08:57 +0100

    Update download sync script

--- bin/download.sh
+++ bin/download.sh
@@ -6,3 +6,5 @@ rsync -av larevuedurable.com:/sites/larevuedurable.com/htdocs/upload/ upload
 rsync -av larevuedurable.com:/sites/larevuedurable.com/htdocs/img/ img
 rsync -av larevuedurable.com:/sites/larevuedurable.com/htdocs/download/ download
 rsync -av larevuedurable.com:/sites/larevuedurable.com/htdocs/download_tmp/ download_tmp
+rsync -av larevuedurable.com:/sites/larevuedurable.com/htdocs/modules/homeslider/images/ modules/homeslider/images
+rsync -av larevuedurable.com:/sites/larevuedurable.com/htdocs/modules/prestablog/themes/default/up-img/ modules/prestablog/themes/default/up-img
