bc7a05899f93cb32a2bcfa45676ccf0d8c4d795f
Sam Baptista <samuel.baptista@ecodev.ch>
2015-02-10 09:55:56 +0100

    Add imported from cresus transfer files to download script

--- bin/download.sh
+++ bin/download.sh
@@ -10,3 +10,4 @@ rsync -av larevuedurable.com:/sites/larevuedurable.com/htdocs/download/ download
 rsync -av larevuedurable.com:/sites/larevuedurable.com/htdocs/download_tmp/ download_tmp
 rsync -av larevuedurable.com:/sites/larevuedurable.com/htdocs/modules/homeslider/images/ modules/homeslider/images
 rsync -av larevuedurable.com:/sites/larevuedurable.com/htdocs/modules/prestablog/themes/default/up-img/ modules/prestablog/themes/default/up-img
+rsync -av larevuedurable.com:/sites/larevuedurable.com/htdocs/modules/ecodevscripts/de_cresus modules/ecodevscripts/de_cresus
