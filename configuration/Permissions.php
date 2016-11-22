cc7f66dae3066e07ff9f2542e67571693c31c158
Samuel Baptista <samuel.baptista@ecodev.ch>
2016-11-22 16:50:59 +0100

    Add configuration file

---
+++ configuration/Permissions.php
@@ -0,0 +1,105 @@
+<?php
+
+return [
+    [
+        'owner' => 'larevuedurablecom-www',
+        'path' => 'htdocs/themes/larevuedurable/cache',
+        'permissions' => '0775',
+        'recursive' => true,
+    ],
+    [
+        'path' => 'htdocs/override',
+        'permissions' => '0777',
+        'recursive' => true,
+    ],
+    [
+        'path' => 'htdocs/sitemap.xml',
+        'permissions' => '0775',
+        'recursive' => true,
+    ],
+    [
+        'permissions' => '0775',
+        'recursive' => true,
+    ],
+    [
+        'path' => 'htdocs/administrator/backups',
+        'permissions' => '0777',
+        'recursive' => true,
+    ],
+    [
+        'path' => 'htdocs/download',
+        'permissions' => '0775',
+        'recursive' => true,
+    ],
+    [
+        'path' => 'htdocs/download_tmp',
+        'permissions' => '0775',
+        'recursive' => true,
+    ],
+    [
+        'path' => 'htdocs/upload',
+        'permissions' => '0775',
+        'recursive' => true,
+    ],
+    [
+        'path' => 'htdocs/translations',
+        'permissions' => '0775',
+        'recursive' => true,
+    ],
+    [
+        'path' => 'htdocs/modules',
+        'permissions' => '0775',
+        'recursive' => true,
+    ],
+    [
+        'path' => 'htdocs/themes/larevuedurable/modules',
+        'permissions' => '0775',
+        'recursive' => true,
+    ],
+    [
+        'path' => 'htdocs/mails',
+        'permissions' => '0775',
+        'recursive' => true,
+    ],
+    [
+        'path' => 'htdocs/img',
+        'permissions' => '0775',
+        'recursive' => true,
+    ],
+    [
+        'path' => 'htdocs/themes/larevuedurable/lang',
+        'permissions' => '0775',
+        'recursive' => true,
+    ],
+    [
+        'path' => 'htdocs/themes/larevuedurable/mails',
+        'permissions' => '0775',
+        'recursive' => true,
+    ],
+    [
+        'path' => 'htdocs/log',
+        'permissions' => '0775',
+        'recursive' => true,
+    ],
+    [
+        'path' => 'htdocs/config',
+        'permissions' => '0775',
+        'recursive' => true,
+    ],
+    [
+        'owner' => 'larevuedurablecom-www',
+        'path' => 'htdocs/cache',
+        'permissions' => '0775',
+        'recursive' => true,
+    ],
+    [
+        'path' => 'htdocs/upload',
+        'permissions' => '0775',
+        'recursive' => true,
+    ],
+    [
+        'path' => 'htdocs/modules/ecodevscripts/data/files',
+        'permissions' => '0775',
+        'recursive' => true,
+    ],
+];
