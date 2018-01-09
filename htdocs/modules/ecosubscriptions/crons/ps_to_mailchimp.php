a432ff77bfa92492b67740786466cc5b7647154e
Samuel Baptista <samuel.baptista@ecodev.ch>
2018-01-09 15:27:34 +0100

    Update path in crons

--- htdocs/modules/ecosubscriptions/crons/ps_to_mailchimp.php
+++ htdocs/modules/ecosubscriptions/crons/ps_to_mailchimp.php
@@ -18,13 +18,13 @@ if ($api->errorCode)
 	$message.= "Unable to Batch subscribe List! - Code = " . $api->errorCode . " - Msg = ".$api->errorMessage;
 
 	echo $message . "\r\n";
-	error_log($message, 3, $_SERVER['DOCUMENT_ROOT'] . '/../logs/cron_log.txt');
+	error_log($message, 3, $_SERVER['DOCUMENT_ROOT'] . '/../../logs/cron_log.txt');
 }
 else
 {
 	$message = $date . ' - MC IMPORT - Success Batch subscribe List!';
 	echo $message . "\r\n";
-	error_log($message . chr(10), 3, $_SERVER['DOCUMENT_ROOT'] . '/../logs/cron_log.txt');
+	error_log($message . chr(10), 3, $_SERVER['DOCUMENT_ROOT'] . '/../../logs/cron_log.txt');
 }
 
 // désinscriptions
@@ -35,7 +35,7 @@ foreach($newsletterUnsub as $s)
 	array_push($unsub, $s['EMAIL']);
 }
 
-if( sizeof($unsub) )
+if( count($unsub) )
 {
 	$vals = $api->listBatchUnsubscribe(_MC_NEWSLETTER_LIST_, $unsub, true, false, false);
 
@@ -45,13 +45,13 @@ if( sizeof($unsub) )
 		$message.= "Unable to Batch unsubscribe List! - Code = " . $api->errorCode . " - Msg = ".$api->errorMessage;
 
 		echo $message . "\r\n";
-		error_log($message . chr(10), 3, $_SERVER['DOCUMENT_ROOT'] . '/../logs/cron_log.txt');
+		error_log($message . chr(10), 3, $_SERVER['DOCUMENT_ROOT'] . '/../../logs/cron_log.txt');
 	}
 	else
 	{
 		$message = $date . " - MC IMPORT -  Success Batch unsubscribe List!";
 
 		echo $message . "\r\n";
-		error_log($message . chr(10), 3, $_SERVER['DOCUMENT_ROOT'] . '/../logs/cron_log.txt');
+		error_log($message . chr(10), 3, $_SERVER['DOCUMENT_ROOT'] . '/../../logs/cron_log.txt');
 	}
 }
