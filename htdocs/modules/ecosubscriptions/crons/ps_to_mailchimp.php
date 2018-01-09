0091147554392f515bf721238681a8a55047a658
Samuel Baptista <samuel.baptista@ecodev.ch>
2018-01-09 17:40:09 +0100

    Update mailchimp newsletter by chunks

--- htdocs/modules/ecosubscriptions/crons/ps_to_mailchimp.php
+++ htdocs/modules/ecosubscriptions/crons/ps_to_mailchimp.php
@@ -5,53 +5,51 @@ require_once(__DIR__ . '/../../../config/config.inc.php');
 require_once(__DIR__ . "/../autoload.php");
 require_once(_PS_MODULE_DIR_ . "/mailchimp/mailchimp.php");
 
-$date = date(_DATE_FORMAT_);
 $mailchimp = new Mailchimp();
-$api  = new MCAPI(_MAILCHIMP_API_KEY_);
+$api = new MCAPI(_MAILCHIMP_API_KEY_);
 
 // inscriptions
-$newsletterSub = Customer::getNewsletterSubscribers();
-$vals = $api->listBatchSubscribe(_MC_NEWSLETTER_LIST_, $newsletterSub, false,true , false);
-if ($api->errorCode)
-{
-	$message = $date . ' - MC IMPORT -  ';
-	$message.= "Unable to Batch subscribe List! - Code = " . $api->errorCode . " - Msg = ".$api->errorMessage;
+$chunks = array_chunk(Customer::getNewsletterSubscribers(), 1000);
 
-	echo $message . "\r\n";
-	error_log($message, 3, $_SERVER['DOCUMENT_ROOT'] . '/logs/cron_log.txt');
-}
-else
-{
-	$message = $date . ' - MC IMPORT - Success Batch subscribe List!';
-	echo $message . "\r\n";
-	error_log($message . chr(10), 3, $_SERVER['DOCUMENT_ROOT'] . '/logs/cron_log.txt');
+foreach ($chunks as $index => $chunk) {
+    $vals = $api->listBatchSubscribe(_MC_NEWSLETTER_LIST_, $chunk, false, true, false);
+    if ($api->errorCode) {
+        var_dump($api);
+        $message = date(_DATE_FORMAT_) . ' - MC IMPORT -  ';
+        $message .= " Chunk " . ($index + 1) . " / " . count($chunks) . ' - ' ;
+        $message .= "Unable to Batch subscribe List! - Code = " . $api->errorCode . " - Msg = " . $api->errorMessage;
+
+        echo $message . "\r\n";
+        error_log($message, 3, $_SERVER['DOCUMENT_ROOT'] . '/logs/cron_log.txt');
+    } else {
+        $message = date(_DATE_FORMAT_) . ' - MC IMPORT -  ';
+        $message .= " Chunk " . ($index + 1) . " / " . count($chunks) . ' - ' ;
+        $message .= " Success Batch subscribe List! " ;
+        echo $message . "\r\n";
+        error_log($message . chr(10), 3, $_SERVER['DOCUMENT_ROOT'] . '/logs/cron_log.txt');
+    }
 }
 
 // désinscriptions
 $newsletterUnsub = Customer::getNewsletterUnsubscribed();
 $unsub = array();
-foreach($newsletterUnsub as $s)
-{
-	array_push($unsub, $s['EMAIL']);
+foreach ($newsletterUnsub as $s) {
+    array_push($unsub, $s['EMAIL']);
 }
 
-if( count($unsub) )
-{
-	$vals = $api->listBatchUnsubscribe(_MC_NEWSLETTER_LIST_, $unsub, true, false, false);
+if (count($unsub)) {
+    $vals = $api->listBatchUnsubscribe(_MC_NEWSLETTER_LIST_, $unsub, true, false, false);
 
-	if ($api->errorCode)
-	{
-		$message = $date . ' - MC IMPORT -  ';
-		$message.= "Unable to Batch unsubscribe List! - Code = " . $api->errorCode . " - Msg = ".$api->errorMessage;
+    if ($api->errorCode) {
+        $message = date(_DATE_FORMAT_) . ' - MC IMPORT -  ';
+        $message .= "Unable to Batch unsubscribe List! - Code = " . $api->errorCode . " - Msg = " . $api->errorMessage;
 
-		echo $message . "\r\n";
-		error_log($message . chr(10), 3, $_SERVER['DOCUMENT_ROOT'] . '/logs/cron_log.txt');
-	}
-	else
-	{
-		$message = $date . " - MC IMPORT -  Success Batch unsubscribe List!";
+        echo $message . "\r\n";
+        error_log($message . chr(10), 3, $_SERVER['DOCUMENT_ROOT'] . '/logs/cron_log.txt');
+    } else {
+        $message = date(_DATE_FORMAT_) . " - MC IMPORT -  Success Batch unsubscribe List!";
 
-		echo $message . "\r\n";
-		error_log($message . chr(10), 3, $_SERVER['DOCUMENT_ROOT'] . '/logs/cron_log.txt');
-	}
+        echo $message . "\r\n";
+        error_log($message . chr(10), 3, $_SERVER['DOCUMENT_ROOT'] . '/logs/cron_log.txt');
+    }
 }
